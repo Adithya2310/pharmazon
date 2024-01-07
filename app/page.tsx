8d8d37a28e90c3a1cb80946f1bc76ae311191a49
Adithya2310 <Adithyanandi099@gmail.com>
2024-01-07 17:57:42 +0530

    Search bar

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import {specialties,doctors,nearByClinics} from "@/lib/data/index";
 import DoctorProfile from '@/components/landingpage/DoctorProfile';
 import NearByClinics from '@/components/landingpage/NearByClinics';
+import Searchbar from '@/components/Searchbar';
 
 export default function Home() {
   return (
@@ -24,34 +25,7 @@ export default function Home() {
         <div className='relative mt-16'>
           <h1 className='w-full text-slate-800 text-6xl font-bold leading-[72px] '>Stay at Home. <br />Consult Online</h1>
           <p className='w-full text-slate-800 text-xl font-normal'>Schedule Your Next Medical Visit Online Today</p>
-          <div className=' flex justify-around items-center mt-32 w-[777.29px] h-[102.38px] bg-transparent bg-opacity-60 rounded-md border-2 border-white backdrop-blur-[35.52px]'>
-            <div className='flex gap-5'>
-              <Image
-                src="/icons/search.svg"
-                alt="search"
-                height={26}
-                width={26}/>
-                <select
-                  className="appearance-none bg-transparent border-none leading-tight h-6 opacity-80 text-slate-500 text-xl font-normal focus:outline-none"
-                  value="Clinic"
-                  defaultValue="Clinic"
-                >
-                  <option value="Clinic">Clinic</option>
-                  {/* <option value="clinic2">Clinic 2</option> */}
-                  {/* <option value="clinic3">Clinic 3</option> */}
-                  {/* Add more options as needed */}
-                </select>
-                <Image
-                  src="/icons/down-arrow.svg"
-                  alt="drop-down"
-                  height={18}
-                  width={21}/>
-            </div>
-              <input type="text" className=" bg-transparent outline-none placeholder:text-xl" placeholder="Search by Clinic name"/>
-              <button className='px-[36.57px] py-[9.40px] bg-amber-500 rounded-md justify-center items-center gap-[10.45px] inline-flex'>
-                Find & Book
-              </button>
-          </div>
+          <Searchbar></Searchbar>
         </div>
         <Image
         className=' -z-10 right-28 absolute'
---
+++ components/Searchbar.tsx
@@ -0,0 +1,46 @@
+import { FormEvent, useState } from "react";
+import {search} from "@/lib/action/search";
+import Image from "next/image";
+
+const Searchbar = () => {
+    const [searchTerm,setSearchTerm] = useState('');
+    //on submission of the form
+    const handleFormSubmit=(e:FormEvent)=>{
+        e.preventDefault();
+        console.log("form submission");
+        search(searchTerm);
+    }
+  return (
+    <div className=' flex justify-around items-center mt-32 w-[777.29px] h-[102.38px] bg-transparent bg-opacity-60 rounded-md border-2 border-white backdrop-blur-[35.52px]'>
+    <div className='flex gap-5'>
+      <Image
+        src="/icons/search.svg"
+        alt="search"
+        height={26}
+        width={26}/>
+        <select
+          className="appearance-none bg-transparent border-none leading-tight h-6 opacity-80 text-slate-500 text-xl font-normal focus:outline-none"
+          value="Clinic"
+          defaultValue="Clinic"
+        >
+          <option value="Clinic">Clinic</option>
+          {/* <option value="clinic2">Clinic 2</option> */}
+          {/* <option value="clinic3">Clinic 3</option> */}
+        </select>
+        <Image
+          src="/icons/down-arrow.svg"
+          alt="drop-down"
+          height={18}
+          width={21}/>
+    </div>
+    <form className="flex" onSubmit={handleFormSubmit}>
+        <input type="text" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} className=" bg-transparent outline-none placeholder:text-xl" placeholder="Search by Clinic name"/>
+    </form>
+    <button onClick={handleFormSubmit} type="submit" className='px-[36.57px] py-[9.40px] bg-amber-500 rounded-md justify-center items-center gap-[10.45px] inline-flex'>
+          Find & Book
+    </button>
+  </div>
+  )
+}
+
+export default Searchbar
